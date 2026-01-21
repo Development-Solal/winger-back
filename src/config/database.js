5cda200b0459a08a4fcd0a68b888b6d375c4b956
Teshand <teshand.r@indigita.ch>
2026-01-21 15:00:05 +0400

    Add conf for build

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: '.env.production' });
-} else {
-  require('dotenv').config();  // Default for development or test
-}
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432, // Default to 5432 if DB_PORT is not set
     dialect: process.env.DB_DIALECT || 'postgres',
   },
   test: {
@@ -18,7 +12,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432, // Default to 5432 if DB_PORT is not set
     dialect: process.env.DB_DIALECT || 'postgres',
   },
   production: {
@@ -26,7 +20,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432, // Default to 5432 if DB_PORT is not set
     dialect: process.env.DB_DIALECT || 'postgres',
   },
 };
