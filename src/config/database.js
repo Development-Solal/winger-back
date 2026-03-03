347bdf01623e415fcd56c6c99e150a2eb37deaf9
noorefatemahsuhabuth <73735067+noorefatemahsuhabuth@users.noreply.github.com>
2026-03-03 14:52:41 +0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -33,6 +33,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
-    dialect: process.env.DB_DIALECT || 'postgres'
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
