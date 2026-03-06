85a12ddce0c31a0e0925770ad045d44f447768db
noorefatemahsuhabuth <73735067+noorefatemahsuhabuth@users.noreply.github.com>
2026-03-06 18:12:01 +0400

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -15,6 +15,8 @@ const setupSocket = (server) => {
                     'https://winger-front.devsolalco.workers.dev',
                     'https://preprod.winger.fr',
                     'https://winger.fr',
+                    'https://www.winger.fr',
+                    'https://www.preprod.winger.fr',
                     'https://dev.winger.fr', // Added
                     'http://localhost:3000', // Added for development
                     'http://localhost:5173', // Added for development
