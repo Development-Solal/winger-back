d902ca48da0688891d171bd4bf34171cdb82c00e
Noore <noorefatemahsuhabuth@Noorefatemahs-MacBook-Air.local>
2026-03-06 14:13:22 +0400

    add strict cookie setting

--- src/controllers/aidant.controller.js
+++ src/controllers/aidant.controller.js
@@ -271,7 +271,7 @@ const setAuthCookie = (res, accessToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "None",
+        sameSite: "Strict",
         maxAge: 4 * 60 * 60 * 1000,
     });
 };
